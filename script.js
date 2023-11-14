75ef382af83a369a734c95d4b0fb40386c599d3e
mohdhammad01 <90841178+mohdhammad01@users.noreply.github.com>
2023-11-14 21:21:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+var tl= gsap.timeline();
+tl.to("#page1", {
+
+    y:"100vh",
+    scale: 0.6,
+    duration:0,
+    delay:0
+
+
+})
+tl.to("#page1",{
+    y:"0vh",
+    duration:1,
+    delay:1.5
+})
+
+tl.to("#page1",{
+
+    rotate:360,
+    scale:1,
+    duration:0.8
+})
+
+
+
+
